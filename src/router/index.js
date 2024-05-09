e2de0891ec53905986358f7fdbe1187c6a5c5a91
김민선 <minsun9856@gmail.com>
2024-05-09 11:08:07 +0900

    feat : FindPwdView 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/user/LoginView.vue";
 import JoinView from "@/views/user/JoinView.vue";
+import FindPwdView from "@/views/user/FindPwdView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/join",
       name: "join",
       component: JoinView,
+    },
+    {
+      path: "/findpwd",
+      name: "findpwd",
+      component: FindPwdView,
     },
   ],
 });
---
+++ src/views/user/FindPwdView.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+		<div class="shadow-sm rounded border p-5 mb-3 position-absolute start-50 translate-middle-x"
+            id="findPwd_div">
+		<form method="POST" action="">
+			<input type="hidden" name="action" value="findpwd"/>
+	
+				<h4 class="mb-4">비밀번호 찾기</h4>
+				<div class="form_group">
+					<div class="d-flex flex-row mb-3">
+						<label style="width: 90px">아이디</label> <input
+							class="p-1 mb-3 ms-3" type="text" id="findPwd_id"
+							style="width: 100%" name="userid" value="" required />
+					</div>
+					<div class="d-flex flex-row mb-3">
+						<label style="width: 90px">이메일</label> <input
+							class="p-1 mb-3 ms-3" type="email" id="findPwd_email"
+							style="width: 100%" name="useremail" value="" required />
+					</div>
+					<div class="d-flex flex-row mb-3">
+						<label style="width: 90px">이름</label> <input class="p-1 mb-3 ms-3"
+							type="text" id="findPwd_name" name="username" style="width: 100%" value=""
+							required />
+					</div>
+					<button class="col-12 btn btn-primary mb-3">
+						비밀번호 찾기</button>
+					<p id="pwd_ans" class="d-flex justify-content-center"></p>
+				</div>
+	
+		</form>
+	</div>
+
+</template>
+
+<style scoped>
+#findPwd_div{
+    position: absolute;
+    top: 130px;
+    width: 45%; 
+}
+</style>
