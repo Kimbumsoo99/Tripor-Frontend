7024031186fd923456670993002678d7fc38efb5
김민선 <minsun9856@gmail.com>
2024-05-09 10:13:48 +0900

    feat : JoinView, LoginView 추가

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import Aside from '@/components/layout/Aside.vue';
 <template>
   <Header/>
   <Aside/>
+  <RouterView/>
 </template>
 
 <style scoped>
@@ -17,7 +18,30 @@ import Aside from '@/components/layout/Aside.vue';
     font-weight: 500;
     font-size: 18px;
     font-style: normal;
-  }
+}
+h4{
+    color:#0077CC;
+    font-weight:900;
+}
+h5{
+    color:#0077CC;
+    font-weight:700;
+}
+.form_group input {
+    font-size: 15px;
+    color: #222222;
+    width: 300px;
+    border: none;
+    border-bottom: solid #aaaaaa 1px;
+    padding-bottom: 20px;
+    padding-left: 20px;
+    position: relative;
+    background: none;
+    z-index: 5;
+}
+
+.form_group input::placeholder { color: #aaaaaa; }
+.form_group input:focus { outline: none; }
 .btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary:visited {
     background-color: #0077CC !important;
 }
