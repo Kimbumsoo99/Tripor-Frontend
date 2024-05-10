5e70bfc65cf971fd6783635de47ce9d1b3e7d6b5
김민선 <minsun9856@gmail.com>
2024-05-10 10:45:01 +0900

    rename : trip -> plan 폴더명 변경

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ import LoginView from "@/views/user/LoginView.vue";
 import JoinView from "@/views/user/JoinView.vue";
 import FindPwdView from "@/views/user/FindPwdView.vue";
 import ProfileView from "@/views/user/ProfileView.vue";
-import MakePlanView from "@/views/trip/MakePlanView.vue";
-import MyPlanView from "@/views/trip/MyPlanView.vue";
+import MakePlanView from "@/views/plan/MakePlanView.vue";
+import MyPlanView from "@/views/plan/MyPlanView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
