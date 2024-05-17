333d73e04126731cf095efe13c91e0394b647222
김민선 <minsun9856@gmail.com>
2024-05-17 15:59:42 +0900

    feat : chatgpt 여행 예산 일정 세우기 구현

--- src/components/board/item/BoardFormItem.vue
+++ src/components/board/item/BoardFormItem.vue
@@ -97,17 +97,6 @@ const aiExpand = () => {
     aiExpanded.value = !aiExpanded.value;
 }
 
-const categoryItems = [
-    { code: 12, name: "관광지" },
-    { code: 14, name: "문화시설" },
-    { code: 15, name: "축제공연행사" },
-    { code: 25, name: "여행코스" },
-    { code: 28, name: "레포츠" },
-    { code: 32, name: "숙박" },
-    { code: 38, name: "쇼핑" },
-    { code: 39, name: "음식점" },
-];
-
 const q1 = ref('');
 const q2 = ref('');
 const q3 = ref('');
