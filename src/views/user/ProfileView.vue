c316efc0104d2fb07f9df0d32603a582f5d55267
김민선 <minsun9856@gmail.com>
2024-05-20 16:15:30 +0900

    feat : 마이페이지 프로필 영역 추가

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -27,6 +27,20 @@ const memberUpdate = async function () {
 		})
 	
 };
+
+const images = ref([]);
+
+const upload = async () => {
+    console.log(images.value.files[0]);
+    await insertImage(
+        images.value.files[0],
+        (res) => {
+            console.log(res);
+            profile.value = `${VITE_UPLOAD_FILE_PATH}/${res.data.fileInfo.saveFolder}/${res.data.fileInfo.saveFile}`;
+        },
+        (err) => console.log(err)
+    );
+};
 </script>
 
 <template>
@@ -41,29 +55,45 @@ const memberUpdate = async function () {
 					</div>
 					<input type="hidden" name="action" value="modify"/>
 					<div class="form_group">
-						<div class="d-flex flex-row mb-3">
-							<label style="width: 90px">이름</label> <input
-								class="p-1 mb-3 ms-3" type="text" id="mypage_name"
-								name="userName"
-								style="width: 100%; text-align: right" 
-								v-model="name"
-								required />
-						</div>
-						<div class="d-flex flex-row mb-3">
-							<label style="width: 90px">이메일</label> <input
-								class="p-1 mb-3 ms-3" type="email" id="mypage_email"
-								name="useremail"
-								style="width: 100%; text-align: right"
-								v-model="email"
-								required />
-						</div>
-						<div class="d-flex flex-row mb-5">
-							<label style="width: 90px">비밀번호</label> <input
-								class="p-1 mb-3 ms-3" type="password" id="mypage_pwd"
-								name="userpwd"
-								style="width: 100%; text-align: right"
-								v-model="pw"
-								required />
+						<div id="profile_div">
+							<div class="d-flex flex-column align-items-center" id="profile_upper" style="position: relative">
+								<div class="profile-image-area">
+									<img :src="userInfo.profile ? userInfo.profile : '/src/assets/image/default_profile_img.png'" id="profileImage" >
+									
+								</div>
+								<div class="profile-btn-area mt-3">
+									<button type="button" class="btn btn-outline-secondary btn-sm me-1">이미지 선택</button>
+									<input type="file" name="profileImage" ref="images" @change="upload" id="imageInput" accept="image/*">
+									<button type="button" class="btn btn-outline-danger btn-sm">삭제</button>
+								</div>
+							</div>
+							<div style="width: 30px"></div>
+							<div class="mt-3" id="info_upper">
+								<div class="d-flex flex-row mb-3">
+									<label style="width: 100px">이름</label> <input
+										class="p-1 mb-3 ms-1" type="text" id="mypage_name"
+										name="userName"
+										style="width: 100%; text-align: right" 
+										v-model="name"
+										required />
+								</div>
+								<div class="d-flex flex-row mb-3">
+									<label style="width: 100px">이메일</label> <input
+										class="p-1 mb-3 ms-1" type="email" id="mypage_email"
+										name="useremail"
+										style="width: 100%; text-align: right"
+										v-model="email"
+										required />
+								</div>
+								<div class="d-flex flex-row mb-5">
+									<label style="width: 100px">비밀번호</label> <input
+										class="p-1 mb-3 ms-1" type="password" id="mypage_pwd"
+										name="userpwd"
+										style="width: 100%; text-align: right"
+										v-model="pw"
+										required />
+								</div>
+							</div>
 						</div>
 						<button class="col-12 btn btn-primary mb-3">정보수정</button>
 						<p id="complete_msg" v-if="isComplete">회원정보 수정이 완료되었습니다.</p>
@@ -82,6 +112,17 @@ const memberUpdate = async function () {
 		width: 45%;
 		margin-left: 100px;
 	}
+	#profile_div{
+		display: flex;
+		flex-direction: row;
+		margin: 0px;
+	}
+	#profile_upper{
+		width: 35%;
+	}
+	#info_upper{
+		width: 65%;
+	}
 }
 @media (max-width: 1199px) {
     #myPage_div{
@@ -89,10 +130,57 @@ const memberUpdate = async function () {
 		top: 130px; 
 		width: 80%;
 	}
+	#profile_div{
+		display: flex;
+		flex-direction: column;
+	}
+	.profile-image-area{
+		width: 100%;
+		display: flex;
+		justify-content: center;
+	}
+
 }
 #complete_msg{
 	text-align: center; 
 	font-size: 16px;
 	color: rgb(220, 0, 0);
 }
+.profile-image-area{
+    width: 150px;
+    height: 150px;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+
+    position: relative;
+
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+#profileImage{
+    height: 100%;
+}
+#deleteImage{
+    position: absolute;
+    top: 10px;
+    right: 20px;
+    cursor: pointer;
+}
+
+/* .profile-btn-area > *{
+    width: 100px;
+    height: 33px;
+    padding: 5px 10px;
+
+    border: 1px solid black;
+    background-color: white;
+    font-size: 14px;
+    cursor: pointer;
+    text-align: center;
+} */
+
+#imageInput{ display: none;}
+
 </style>
